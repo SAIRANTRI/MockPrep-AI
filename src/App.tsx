57277bc5076621526463a59ed88b017fc61be9da
SAIRANTRI <pathaksairantri@gmail.com>
2025-08-22 13:31:08 +0530

    Routing setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-export default function App() {
+import { PublicLayout } from "@/layouts/public-layout";
+import AuthenticationLayout from "@/layouts/auth-layout";
+
+
+import HomePage from "@/routes/home";
+import { SignInPage } from "./routes/sign-in";
+import { SignUpPage } from "./routes/sign-up";
+import ProtectRoutes from "./layouts/protected-routes";
+
+
+const App = () => {
   return (
-    <div >
-      <Button className=" text-blue-500">Click me</Button>
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        {/* public routes */}
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+        {/* authentication layout */}
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
+        </Route>
+
+        {/* protected routes */}
+        <Route
+          element={
+            <ProtectRoutes>
+              <MainLayout />
+            </ProtectRoutes>
+          }
+        ></Route>
+        
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
