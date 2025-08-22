2ffc42e58d11c5975b1bb0a47d4f6b2b26429f9f
SAIRANTRI <pathaksairantri@gmail.com>
2025-08-22 09:27:27 +0530

    proejct initialised

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function App() {
+  return (
+    <div >
+      <Button className=" text-blue-500">Click me</Button>
+    </div>
+  )
+}
