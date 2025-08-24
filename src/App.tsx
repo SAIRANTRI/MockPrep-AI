e46395ced97fae14c015044d1469b350f797c5b7
SAIRANTRI <pathaksairantri@gmail.com>
2025-08-24 12:57:25 +0530

    added form and UI components for mock interviews for generating/editing new form for interview
    new libraries.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { PublicLayout } from "@/layouts/public-layout";
 import AuthenticationLayout from "@/layouts/auth-layout";
-
+import ProtectRoutes from "@/layouts/protected-routes";
+import { MainLayout } from "@/layouts/main-layout";
 
 import HomePage from "@/routes/home";
 import { SignInPage } from "./routes/sign-in";
 import { SignUpPage } from "./routes/sign-up";
-import ProtectRoutes from "./layouts/protected-routes";
-import { MainLayout } from "./layouts/main-layout";
+import { Generate } from "./components/generate";
+import { Dashboard } from "./routes/dashboard";
+import { CreateEditPage } from "./routes/create-edit-page";
+
 
 const App = () => {
   return (
@@ -32,8 +35,14 @@ const App = () => {
               <MainLayout />
             </ProtectRoutes>
           }
-        ></Route>
-        
+        >
+          {/* add all the protect routes */}
+          <Route element={<Generate />} path="/generate">
+            <Route index element={<Dashboard/>} />
+            <Route path=":interviewId" element={<CreateEditPage />} />
+          </Route>
+            
+        </Route>
       </Routes>
     </Router>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react'
   import './index.css'
   import App from './App.tsx'
   import { ClerkProvider } from '@clerk/clerk-react'
+import { ToasterProvider } from './provider/toast-provider.tsx'
 
   // Import your Publishable Key
   const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -15,6 +16,7 @@ import { StrictMode } from 'react'
     <StrictMode>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <App />
+        <ToasterProvider />
       </ClerkProvider>
     </StrictMode>,
   )
